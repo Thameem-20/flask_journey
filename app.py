8d17912934c426a0e32ef79b8361bfbf2e8c5ca2
Thameem-20 <ahmedthameem20@gmail.com>
2024-01-24 23:11:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello world'
+    return render_template('index.html')
+
+@app.route('/About')
+def About():
+    return 'This is about page'
+
+@app.route('/Contact')
+def Contact():
+    return 'This is Contact page'
 
 if __name__ == "__main__":
     app.run(debug=True)
