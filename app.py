04fdc08d6d14a9a66db0d9c57632bf608dacca5e
Thameem-20 <ahmedthameem20@gmail.com>
2024-01-26 22:19:12 +0530

    third commit

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE-URI']= "sqlite:////todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(200), nullable=False)
-    desc=db.Column(db.String(500), nullable=False)
-    created_date=db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"{self.sno}-{self.title}"
+        return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo=Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
 
-@app.route('/About')
-def About():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/update')
+def update():
+    allTodo = Todo.query.all()
+    print(allTodo)
     return 'This is about page'
 
-@app.route('/Contact')
-def Contact():
-    return 'This is Contact page'
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
